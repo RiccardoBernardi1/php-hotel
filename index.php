a90554bf751321cb0db5e5ee080c082d2e308c12
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-12-14 14:40:21 +0100

    Crea tabella con tutti gli hotel e le relative info

--- index.php
+++ index.php
@@ -48,28 +48,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>PHP Hotel</title>
 </head>
 <body>
-    <h1>Lista di Hotel</h1>
-    <ul>
-        <?php foreach($hotels as $hotel){ ?>
-            <li>
-                <h3><?php echo $hotel['name'] ?></h3>
-                <p><?php echo $hotel['description'] ?></p>
-                <div>
-                    <?php 
-                        if($hotel['parking']==true){
-                            echo "Si";
-                        }else{
-                            echo "No";
-                        }
-                    ?>
-                </div>
-                <div><?php echo $hotel['vote'] ?></div>
-                <div><?php echo $hotel['distance_to_center'] ?></div>
-            </li>
-        <?php } ?>
-    </ul>
+    <div class="container pt-5">
+        <table class="table table-striped-columns table-dark text-center mt-5">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro (Km)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($hotels as $hotel){?>
+                    <tr>
+                        <?php foreach($hotel as $key => $info){
+                            if($key==="name"){
+                                echo "<th scope=\"row\">$info</th>";
+                            }elseif($key==="parking"){
+                                if($info){
+                                    echo "<td>Si</td>";
+                                }else{
+                                    echo "<td>No</td>";
+                                }
+                            }else{
+                                echo "<td>$info</td>";
+                            }
+                        } ?>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
 </html>
