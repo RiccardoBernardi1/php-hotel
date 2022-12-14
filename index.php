af461abfe646e515ea7c93b49c0ad88fa52d1f42
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-12-14 14:04:33 +0100

    Stampa a schermo tutti i dati di ogni hotel

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+</head>
+<body>
+    <h1>Lista di Hotel</h1>
+    <ul>
+        <?php foreach($hotels as $hotel){ ?>
+            <li>
+                <h3><?php echo $hotel['name'] ?></h3>
+                <p><?php echo $hotel['description'] ?></p>
+                <div>
+                    <?php 
+                        if($hotel['parking']==true){
+                            echo "Si";
+                        }else{
+                            echo "No";
+                        }
+                    ?>
+                </div>
+                <div><?php echo $hotel['vote'] ?></div>
+                <div><?php echo $hotel['distance_to_center'] ?></div>
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
